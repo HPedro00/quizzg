9ea11b0588e6be576acd993d59a7bca0b0c04657
HPedro00 <140410034+HPedro00@users.noreply.github.com>
2023-11-13 10:20:45 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,5 @@
-// creating an array and passing the number, questions, options, and
-answers
 let questions = [
-{
+  {
 numb: 1,
 question: "que dia aconteceu o atentado as torres gemeas?"
 answer: "11/9",
@@ -33,7 +31,7 @@ options: [
 "China",
 "India",
 "Russia"
-]
+  ]
 },
 {
 numb: 4,
@@ -60,19 +58,18 @@ options: [
 "jupter"
 ]
 },
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
