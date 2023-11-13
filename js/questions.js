51ce53b7d6541d6c16e6752ca8412b0bffdc4971
HPedro00 <140410034+HPedro00@users.noreply.github.com>
2023-11-13 10:27:20 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,63 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
 let questions = [
   {
-numb: 1,
-question: "que dia aconteceu o atentado as torres gemeas?"
-answer: "11/9",
-options: [
-"11/9",
-"11/8",
-"7/9",
-"7/8"
-]
-},
-{
-numb: 2,
-question: "Qual o menor país do mundo?",
-answer: "Vaticano",
-options: [
-"Vaticano",
-"Montenegro",
-"Sudão do Sul",
-"Kosovo"
-]
-},
-{
-numb: 3,
-question: "
-mais longo do ano?"Qual o país mais grande do mundo?"
-answer: "Russia",
-options: [
-"Brasil",
-"China",
-"India",
-"Russia"
-  ]
-},
-{
-numb: 4,
-question: "qual a capital da Russia?"
-possui?",
-answer: Moscou",
-options: [
-"Moscou",
-"Brasilia",
-"Lisboa",
-"Toquio"
-]
-
-},
-{
-numb: 5,
-question: "Qual o maior planeta do sistema solar?"
-rotação em sentido horário?",
-answer: "jupter",
-options: [
-"Saturno ",
-"Vênus",
-"Terra ",
-"jupter"
-]
-},
+    numb: 1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Curiee",
+    options: [
+      "Albert Einstein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Qual o país mais novo do mundo?",
+    answer: "Sudão do Sul",
+    options: [
+      "Timor Lestet",
+      "Montenegro",
+      "Sudão do Sul",
+      "Kosovo"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
+    answer: "Solstício de verão",
+    options: [
+      "Equinócio de outono",
+      "Solstício de inverno",
+      "Solstício de verão",
+      "Solstício de outono"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
+    answer: "12",
+    options: [
+      "12",
+      "11",
+      "14",
+      "13"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
+    answer: "Vênus e Urano",
+    options: [
+      "Saturno e Júpiter",
+      "Vênus e Urano",
+      "Terra e Marte",
+      "Mercúrio e Saturno"
+    ]
+  },
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
